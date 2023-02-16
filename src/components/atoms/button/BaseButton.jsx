c3c43292a317a39a72828e7078fc847b734b32f3
otter-botter-417 <124283314+otter-botter-417@users.noreply.github.com>
2023-02-16 20:29:02 +0900

    Initial commit

---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  outline: none;
+  border-radius: 9999px;
+  border: none;
+  &:hover {
+    corsor: pointer;
+    opacity: 0.8;
+  }
+`;
